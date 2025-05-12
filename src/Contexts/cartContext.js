ccce15a1ef901abe3e11620cda738bd3cf8c1ebc
Joao Santos <jmsdo1970@gmail.com>
2025-05-12 11:55:06 -0300

    adicao e remocao de item no carrinho

--- src/Contexts/cartContext.js
+++ src/Contexts/cartContext.js
@@ -30,11 +30,35 @@ function CartProvider({children}){
         console.log([...cart, data])
     }
 
+    function removeItemCart(product){
+        // percorre a lista, assim retornando a posicao desse item
+        const indexItem = cart.findIndex(item => item.id === product.id)
+
+        if(cart[indexItem]?.amount > 1) {
+            let cartList = cart;    
+
+            // pega essa posicao da lista e diminui
+            cartList[indexItem].amount = cartList[indexItem].amount - 1
+
+            // recalcula o preco total desse produto
+            cartList[indexItem].total = cartList[indexItem].amount - cartList[indexItem].price
+
+            setCart(cartList)
+            return;
+
+        }
+        // o filter vai retornar todos os itens e guardar nessa variavel
+        const removeItem = cart.filter(item => item.id !== product.id)
+        setCart(removeItem)
+
+    }
+
     return(
         <CartContext.Provider
             value={{
                 cart,
-                addItemCart
+                addItemCart,
+                removeItemCart
             }}
         >
             {children}
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,15 +1,38 @@
-import { View, Text, StyleSheet } from "react-native"
+import { useContext } from "react"
+import { View, Text, StyleSheet, FlatList } from "react-native"
+
+import { CartContext } from "../../Contexts/cartContext"
+import CardItem from "../../Components/CardItem";
 
 export default function Cart(){
+
+    const {cart, addItemCart, removeItemCart} = useContext(CartContext);
+
     return(
         <View style={styles.container}>
-            <Text>Pagina Carrinho</Text>
+            <FlatList
+            data={cart}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={ () => <Text>Nenhum item no carrinho..</Text>}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={({item}) => (
+                <CardItem
+                    data={item}
+                    addAmount={ ()=> addItemCart(item)}
+                    removeAmount = { () => removeItemCart(item)}
+                />
+            )}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex:1
+        flex:1,
+        backgroundColor:'#fafafa',
+        paddingStart:14,
+        paddingEnd:14,
+        paddingTop:14
     }
 })
